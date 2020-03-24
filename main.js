2d45774f0518b106aba65ce03500b060d611fba0
Wandering Crow <FraternaliAlessandro@gmail.com>
2020-03-24 20:52:27 +0100

    Inserite var, iniziato for

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-alert("culo")
+// richiesta del cognome da inserire
+var nuovo = prompt("Quale nuovo cognome vuoi inserire?");
+
+// array di cognomi
+var lista = ["Diplodoco", "Kebab", "Canaglia", "Enciclopedico", "Scarponi", "Bonobo"]
+
+// inserimento del cognome utente all'interno dell'array
+lista.push(nuovo);
+var presente;
+
+for (var i = 0; i < lista.length; i++) {
+  presente = lista[i];
+  console.log(presente);
+}
+
+// stampa dei cognomi in ordine alfabetico
+
+
+// stampa della posizione del cognome utente
