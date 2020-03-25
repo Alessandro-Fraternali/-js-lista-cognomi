ceb6d6e699fe5314bd0d1d906dcd273911360d48
Wandering Crow <FraternaliAlessandro@gmail.com>
2020-03-25 17:35:56 +0100

    Esercizio concluso

--- main.js
+++ main.js
@@ -4,16 +4,31 @@ var nuovo = prompt("Quale nuovo cognome vuoi inserire?");
 // array di cognomi
 var lista = ["Diplodoco", "Kebab", "Canaglia", "Enciclopedico", "Scarponi", "Bonobo"]
 
-// inserimento del cognome utente all'interno dell'array
-lista.push(nuovo);
-var presente;
-
+// stampa dell'array di cognomi 
+var cognomePrecedente;
 for (var i = 0; i < lista.length; i++) {
-  presente = lista[i];
-  console.log(presente);
+  cognomePrecedente = document.getElementById("cognomibase").innerHTML ;
+  document.getElementById("cognomibase").innerHTML = cognomePrecedente + "<li>" + lista[i] +"</li>";
 }
 
-// stampa dei cognomi in ordine alfabetico
+// inserimento del cognome utente all'interno dell'array
+lista.push(nuovo);
 
+// stampa dei cognomi in ordine alfabetico
+lista.sort();
+
+var cognomePrecedente2;
+
+for (var i = 0; i < lista.length; i++) {
+  cognomePrecedente2 = document.getElementById("stampacognomi").innerHTML ;
+  document.getElementById("stampacognomi").innerHTML = cognomePrecedente2 + "<li>" + lista[i] +"</li>";
+}
 
 // stampa della posizione del cognome utente
+var listaOrdineAlfabetico = lista.indexOf(nuovo);
+
+console.log(listaOrdineAlfabetico);
+
+var posizione = listaOrdineAlfabetico +1;
+
+document.getElementById("dichiarazione").innerHTML =posizione;
